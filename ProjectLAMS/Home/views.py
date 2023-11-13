215bbfe2da622914678bb8280330c9e97726085d
sibims <sibima00@gmail.com>
2023-11-13 18:58:15 +0530

    two urls path are created.one for home page and another for the attendance records page

--- ProjectLAMS/Home/views.py
+++ ProjectLAMS/Home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Render the front page
+def front(request):
+    # Render a template named 'attendance/front.html'
+    return render(request, 'home.html')
+
+# Render the index page
+def attendancerecords(request):
+    # Render a template called 'attendance/index.html'
+    return render(request, 'table.html')
